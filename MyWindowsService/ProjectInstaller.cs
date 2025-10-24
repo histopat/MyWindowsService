0c50d771512a46da8d3725ca3869b6341d892cf8
Erdem Topalsalih <erdem_topalsalih@hotmail.com.tr>
2025-10-24 21:11:11 +0300

    Proje dosyası ekle.

---
+++ MyWindowsService/ProjectInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+using System.Configuration.Install;
+
+[RunInstaller(true)]
+public class ProjectInstaller : Installer
+{
+    public ProjectInstaller()
+    {
+        var processInstaller = new ServiceProcessInstaller();
+        var serviceInstaller = new ServiceInstaller();
+
+        processInstaller.Account = ServiceAccount.LocalSystem;
+
+        serviceInstaller.ServiceName = "MyWindowsService";
+        serviceInstaller.DisplayName = "My Windows Service";
+        serviceInstaller.StartType = ServiceStartMode.Manual;
+
+        Installers.Add(processInstaller);
+        Installers.Add(serviceInstaller);
+    }
+}
