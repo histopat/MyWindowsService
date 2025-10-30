4083f88356ebd0d9e8cfd1db66fb466b1e6b8ce1
Erdem Topalsalih <erdem_topalsalih@hotmail.com.tr>
2025-10-30 16:53:48 +0300

    Proje dosyası ekle.

---
+++ MyWindowsService/ProjectInstaller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ServiceProcess;
+using System.Configuration.Install;
+
+[RunInstaller(true)]
+public class ProjectInstaller : Installer
+{
+    public ProjectInstaller()
+    {
+        var processInstaller = new ServiceProcessInstaller();
+        var serviceInstaller = new ServiceInstaller();
+
+        processInstaller.Account = ServiceAccount.LocalSystem;
+
+        serviceInstaller.ServiceName = "MyWindowsService";
+        serviceInstaller.DisplayName = "My Windows Service";
+        serviceInstaller.StartType = ServiceStartMode.Manual;
+
+        Installers.Add(processInstaller);
+        Installers.Add(serviceInstaller);
+    }
+}
---
+++ MyWindowsService/Service1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+            this.ServiceName = "MyWindowsService";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                MessageBox.Show("Hello, this message comes from a Windows service!", "My Windows Service", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.WriteAllText(@"C:\ServiceLog.txt", ex.ToString());
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
